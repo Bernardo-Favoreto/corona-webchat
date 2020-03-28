750425fbccbaea144922f0b2d309d9671e679a61
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-03-27 21:56:25 -0300

    third commit

---
+++ public/assets/webchat-appConfig.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "AC0f76796468ddaba56cc9ef60430193ee",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO10648573a6295ea1fbc6c11b772d0108",
+    colorTheme: {
+        overrides: brandedColors
+    }
+}
