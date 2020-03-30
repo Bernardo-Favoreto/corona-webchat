1537ca311309c242648597523e4fea975cade24e
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-03-30 10:58:03 -0300

    app teste

--- src/App.js
+++ src/App.js
@@ -7,22 +7,41 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    FlexWebChat.MainHeader.defaultProps.showImage = false;
+    FlexWebChat.MainContainer.defaultProps.height = "500px"
+    FlexWebChat.MainContainer.defaultProps.width = "350px"
+    FlexWebChat.MainHeader.defaultProps.titleText = "Fale com o CoronaChat!"
+    FlexWebChat.EntryPoint.defaultProps.tagline = "Fale com o CoronaChat!"
+    FlexWebChat.MessagingCanvas.defaultProps.memberDisplayOptions = {
+      yourDefaultName:"Usuário",
+      theirDefaultName:"CoronaChat",
+      yourFriendlyNameOverride: false,
+      theirFriendlyNameOverride: false
+    }
+
 
     const { configuration } = props;
     FlexWebChat.Manager.create(configuration)
-      .then(manager => this.setState({ manager }))
+      .then(manager => {
+        manager.strings.WelcomeMessage = "Bem-vindo ao CoronaChat :)";
+        manager.strings.PredefinedChatMessageAuthorName = "CoronaChat";
+        manager.strings.PredefinedChatMessageBody = "Pode digitar qualquer coisa pra gente começar a conversar!";
+        manager.strings.Today = "HOJE";
+        manager.strings.Yesterday = "ONTEM";    
+        manager.strings.InputPlaceHolder = "Digite sua mensagem"    
+        
+
+        this.setState({ manager })})
       .catch(error => this.setState({ error }));
-  }
+  };
+
 
   render() {
     const { manager, error } = this.state;
-
-    FlexWebChat.MessagingCanvas.defaultProps.predefinedMessage = false;
-
     if (manager) {
       return (
         <div>
-          <div> Olá, meu site </div>
+          <div>Testando!</div>
           <FlexWebChat.ContextProvider manager={manager}>
             <FlexWebChat.RootContainer />
           </FlexWebChat.ContextProvider>
@@ -39,3 +58,4 @@ class App extends React.Component {
 }
 
 export default App;
+
